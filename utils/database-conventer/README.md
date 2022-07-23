632828049e09673815e011171ef31ffc4205b01c
gnehs <jayuiop@gmail.com>
2022-07-23 11:25:15 +0800

    init w/ vite + vue 3

---
+++ utils/database-conventer/README.md
@@ -0,0 +1,8 @@
+# 資料庫轉換器
+將郵局資料庫轉換為 json 格式
+## 取得
+- 你可以在郵局網站取得 [3+3郵遞區號應用系統](https://www.post.gov.tw/post/internet/Download/all_list.jsp?ID=2201#dl_link_2735)
+- 安裝後可以在 `C:\Zip33U\DBF\rall1.dbf` 取得資料庫檔案
+- 將資料庫檔案放在 `./src` 之下
+- 終端機輸入 `node index.js` 進行轉換
+
---
+++ utils/database-conventer/index.js
@@ -0,0 +1,32 @@
+const XLSX = require("xlsx");
+const fs = require('fs');
+const path = require('path');
+
+let DatabasePath = './src/rall1.dbf'
+console.log(`DatabasePath: ${DatabasePath}`);
+
+let workbook = XLSX.readFile(DatabasePath);
+let parsedData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+parsedData = parsedData.map(item => ({
+  zipcode: item.ZIPCODE,
+  city: item.CITY,
+  area: item.AREA,
+  road: item.ROAD,
+  scope: item.SCOOP,
+}))
+let result = {}
+parsedData.forEach(item => {
+  if (!result[item.city])
+    result[item.city] = {}
+  if (!result[item.city][item.area])
+    result[item.city][item.area] = {}
+  if (!result[item.city][item.area][item.road])
+    result[item.city][item.area][item.road] = []
+
+  result[item.city][item.area][item.road].push({
+    scope: item.scope.trim(),
+    zipcode: parseInt(item.zipcode)
+  })
+})
+fs.writeFileSync(path.join(__dirname, './result/rall1.json'), JSON.stringify(result));
+console.log(`${parsedData.length} items saved to ./result/rall1.json`);
