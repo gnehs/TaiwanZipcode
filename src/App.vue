1bb14c6c1888531be095cd10cf305b52832c29bb
gnehs <jayuiop@gmail.com>
2022-07-23 19:36:11 +0800

    updated page style

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,22 @@
 
 <template>
   <div class="container">
-    <h1>台灣 3+3 郵遞區號查詢</h1>
+    <h1 class="page-title">台灣 3+3 郵遞區號查詢</h1>
     <vue3-simple-typeahead
       class="search-input"
       :items="Object.keys(addressList)"
-      placeholder="輸入全部或部分地址來查詢"
+      placeholder="輸入地址、路名或是鄉鎮市區來查詢"
       @selectItem="selectItem"
       v-model="addressInput" />
-    <div class="result-items">
+    <div class="result-items" v-if="resultList.length">
+      <div class="result-item-header">
+        <div class="result-item-zipcode">
+          郵遞區號
+        </div>
+        <div class="result-item-scope">
+          範圍
+        </div>
+      </div>
       <div class="result-item" v-for="item of resultList">
         <div class="result-item-zipcode">
           {{ item.zipcode }}
@@ -21,34 +29,42 @@
           {{ item.scope }}
         </div>
       </div>
-
     </div>
   </div>
 </template>
 <style lang="sass">
 .container
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
   width: 95%
   max-width: 768px
-  margin: 10px auto
-  padding: 16px
-  border-radius: 8px
-  background-color: #fff
+  margin: auto
+  margin-top: 128px
+.page-title
+  text-align: center
+  font-size: 36px
+  margin: 1em 0
 .search-input
   border: none
-  border-bottom: 1px solid #ccc
+  background-color: #fff
+  border: 1px solid #ccc
+  padding: 16px
   width: 100%
   font-size: 24px
   &:focus
     outline: none
-    border-bottom: 1px solid #000
+    border: 1px solid #000
 .result-items
   margin-top: 16px
-  gap: 8px
-  .result-item
+  background-color: #fff
+  border: 1px solid #ccc
+  .result-item-header
+    font-weight: bold
+    background-color: rgba(0,0,0,.05)
+  .result-item,.result-item-header
     padding: 8px
     display: grid
-    grid-template-columns: 4em 1fr
+    grid-template-columns: 6rem 1fr
+    &:not(:first-child)
+      border-top: 1px solid #ccc
     .result-item-zipcode
       font-weight: 700
 
