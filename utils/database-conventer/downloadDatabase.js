e8c66eb4e56b0a26bc03ee89fe32428d9291ffc5
gnehs <jayuiop@gmail.com>
2023-03-30 16:48:50 +0800

    Refactor downloadDatabase.js to use arrow function and remove unnecessary child_process import

--- utils/database-conventer/downloadDatabase.js
+++ utils/database-conventer/downloadDatabase.js
@@ -1,10 +1,8 @@
-
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const fs = require('fs');
-const { exec } = require('child_process');
-async function main() {
-  let url = 'https://www.post.gov.tw/post/internet/Download/all_list.jsp?ID=2201#dl_link_2735';
+(async () => {
+  let url = 'https://www.post.gov.tw/post/internet/Download/all_list.jsp?ID=2201';
   let response = await fetch(url);
   let text = await response.text();
   let dom = new JSDOM(text);
@@ -15,5 +13,4 @@ async function main() {
   let file = await fetch(link).then(x => x.arrayBuffer())
   fs.writeFileSync('./temp/installer.exe', Buffer.from(file));
   console.log('installer.exe saved to ./temp/installer.exe');
-}
-main();
+})();
