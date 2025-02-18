dec0b581bae7ff36e09a7e23a23d98393a3d5be9
gnehs <gnehs@gnehs.net>
2025-02-18 16:26:25 +0800

    update updateDatabase workflow

--- utils/database-conventer/downloadDatabase.js
+++ utils/database-conventer/downloadDatabase.js
@@ -1,15 +1,46 @@
-const { JSDOM } = require('jsdom');
-const fs = require('fs');
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
+const extract = require("extract-zip");
+const path = require("path");
+const { execSync } = require("child_process");
 (async () => {
-  let url = 'https://www.post.gov.tw/post/internet/Download/all_list.jsp?ID=2201';
+  let url =
+    "https://www.post.gov.tw/post/internet/Download/all_list.jsp?ID=2201";
   let response = await fetch(url);
   let text = await response.text();
   let dom = new JSDOM(text);
-  let link = dom.window.document.querySelector('#dl_link_2735 a').href;
-  console.log(`download link: ${link}`);
+  let links = dom.window.document.querySelectorAll(
+    `#dl_link_2735 a[title^="3+3郵遞區號應用系統"][href$=".zip"]`
+  );
+  // download all links
+  const tempDir = path.resolve("./temp");
+  fs.mkdirSync(tempDir, { recursive: true });
+  let zipPath;
+  for (let link of [...links]) {
+    console.log(`download link: ${link}`);
+    let url = link.href;
+    let response = await fetch(url);
+    let file = await response.arrayBuffer();
+    let filename = url.split("/").pop();
+    zipPath = path.join("./temp", decodeURIComponent(filename));
+    fs.writeFileSync(zipPath, Buffer.from(file));
 
-  fs.mkdirSync('./temp', { recursive: true });
-  let file = await fetch(link).then(x => x.arrayBuffer())
-  fs.writeFileSync('./temp/installer.exe', Buffer.from(file));
-  console.log('installer.exe saved to ./temp/installer.exe');
+    await extract(zipPath, {
+      dir: tempDir,
+    });
+    // delete original zip file
+    fs.unlinkSync(zipPath);
+  }
+  // cat test.zip* > ~/test.zip
+  execSync(`cat ${tempDir}/* > ${path.join(tempDir, "installer.zip")}`);
+  execSync(`unzip ${path.join(tempDir, `installer.zip`)} -d ${tempDir}`);
+
+  // find rename .exe file
+  const files = fs.readdirSync(tempDir);
+  const exeFile = files.find((file) => file.endsWith(".exe"));
+  fs.renameSync(
+    path.join(tempDir, exeFile),
+    path.join(tempDir, "installer.exe")
+  );
+  console.log("installer.exe saved to ./temp/installer.exe");
 })();
